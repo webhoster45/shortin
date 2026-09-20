9a504f69e6197d41aa30ece2272d17b80387dae7
webhoster45 <gbengaayoolawale@gmail.com>
2026-09-20 22:32:17 +0100

    Redis app configuring

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ const port = process.env.PORT || 3000;
 const mongoose=require('mongoose');
 const { type } = require('os');
 const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-const {createClient}=require('redisclient');
+const {createClient}=require('redis');
 const { Socket } = require('dgram');
+const MONGODB_URL=process.env.MONGODB_URL;
+const JWT_SECRET=process.env.JWT_SECERT;
+
 
 const redisoptions={
 	url:process.env.REDIS_URL,
@@ -26,11 +29,23 @@ function requireredis(req,res,next){
 	return res.status(503).json({message:'Redis is unavaliable'})
 }
 
+async function startapp(){
+	if(!MONGODB_URL || !JWT_SECRET) { console.log('MONGODB_URL AND JWT_SECRET ARE REQUIRED'); return;}
+	Promise.all([
+		redispublisher.connect(),
+		redissubscriber.connect(),
+		mongoose.connect(MONGODB_URL).then(()=>console.log('MongoDB connected'))
+	]);
+    app.listen(port, () => {
+	console.log(`Shortin is running at Port: ${port}`);
+});
+}
+
 const Surlschema=mongoose.Schema({
     original_url:{type:String,required:true},
     shortcode:{type:Number,required:true},
 	clickcount:{type:Number}
-})
+},{timestamps:true})
 
 const Surl=mongoose.model('Surl',Surlschema)
 
@@ -43,20 +58,18 @@ function generatelogic(){
 	return string;
 }
 
-function requireredis(){
-	
-}
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
+
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'code.html'));
 });
 
+app.post('/shorten',requireredis,async (req,res)=>{
+    
+})
 
-
-app.listen(port, () => {
-	console.log(`Shortin is running at http://localhost:${port}`);
-});
+startapp();
